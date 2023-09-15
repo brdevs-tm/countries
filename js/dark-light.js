387d3cb1ca5e77295c98b04b43e83940a316fd33
brdevs-tm <bright19.off@gmail.com>
2023-09-15 05:57:08 +0500

    big commit

--- js/dark-light.js
+++ js/dark-light.js
@@ -1,13 +1,17 @@
 const lightMode = document.querySelector(".light-mode");
 const darkMode = document.querySelector(".dark-mode");
 
+const returnPageBtn = document.querySelector(".return-link img");
+
 lightMode.addEventListener("click", () => {
   lightMode.classList.add("mode-hide");
   darkMode.classList.remove("mode-hide");
   document.body.classList.add("white");
+  returnPageBtn.src = "../assets/dark-back.svg";
 });
 darkMode.addEventListener("click", () => {
   darkMode.classList.add("mode-hide");
   lightMode.classList.remove("mode-hide");
   document.body.classList.remove("white");
+  returnPageBtn.src = "../assets/back.svg";
 });
--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
 const countriesRow = document.querySelector(".countries-row");
+const url = "https://ap-countries-api.vercel.app/all?page=1&limit=10";
 
-const ENDPOINT = "https://ap-countries-api.vercel.app/";
+function countryData(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.send();
 
+  request.onreadystatechange = function () {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        let dataJson = request.response;
+        let data = JSON.parse(dataJson);
+        callback(data, null);
+      } else {
+        const error = new Error("Error " + request.status);
+        callback(null, error);
+      }
+    }
+  };
+}
+
+countryData(url, function (data, error) {
+  if (error) {
+    console.error("Error:", error);
+  } else {
+    data.forEach((country) => {
+      countriesRow.innerHTML += `<div class="country-card">
+              <div class="card-body"><img src="${country.flags.png}" alt="${country.name}" /></div>
+              <div class="card-footer">
+                <div class="country-name"><span>${country.name.common}</span></div>
+                <div class="about-country">
+                  <h1>Population: <span>${country.population}</span></h1>
+                  <h1>Region: <span>${country.region}</span></h1>
+                  <h1>Capital: <span>${country.capital[0]}</span></h1>
+                </div>
+              </div>
+            </div> `;
+    });
+  }
+});
